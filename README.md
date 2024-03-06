fa40463b3b32599ac5492699ecdf2763967481f9
ambma <108054357+ambma@users.noreply.github.com>
2024-03-06 11:04:28 +0600

    FIx

--- README.md
+++ README.md
@@ -1,23 +1,42 @@
-# Bincry Consumer
+# BincryInteract
 
-A Node.js module consuming the bincry-interact package for interacting with Binance API and Ethereum smart contracts.
-
-
-Important Note
-Make sure to replace placeholders like YOUR_BINANCE_API_KEY, YOUR_BINANCE_API_SECRET, 0xContractAddress, 0xYourPrivateKey, and yourSmartContractFunction with your actual values before running the consumer logic.
+`BincryInteract` is a Node.js package designed for advanced interactions with Binance's API and the Ethereum blockchain, enabling users to fetch account information, balances, and interact with smart contracts.
 
 ## Installation
 
+To install `BincryInteract`, run the following command in your project directory:
+
 ```bash
-npm install bincry-consumer
+npm install bincry-interact
+```
+
+## Usage
+
+```bash
+const BincryInteract = require('bincry-interact');
+
+// Initialize with your API keys and Ethereum node URL
+const bincry = new BincryInteract('yourBinanceApiKey', 'yourBinanceApiSecret', 'yourEthereumNodeUrl');
+
+// Fetch Binance account info
+bincry.getBinanceAccountInfo().then(info => console.log(info)).catch(err => console.error(err));
+
+// Get Ethereum balance
+bincry.getEthBalance('0x...').then(balance => console.log(balance)).catch(err => console.error(err));
+
+// Interact with a smart contract
+bincry.interactWithSmartContract('0x...', 'yourPrivateKey', 'functionName', ['arg1', 'arg2'])
+  .then(receipt => console.log(receipt))
+  .catch(err => console.error(err));
+  ```
 
 
-USAGE
+## Important Notes
+Ensure your API keys and private keys are securely stored and never hard-coded directly into your applications
 
-const BincryConsumer = require('bincry-consumer');
+The package assumes familiarity with Ethereum smart contracts
 
-// Create an instance of BincryConsumer
-const bincryConsumer = new BincryConsumer();
 
-// Run the consumer logic
-bincryConsumer.runConsumerLogic();
+
+
+
--- bincry-interact.js
+++ bincry-interact.js
@@ -1,33 +1,95 @@
-// bincry-consumer.js
-const BincryInteract = require('bincry-interact');
+const axios = require('axios');
+const Web3 = require('web3');
 
-class BincryConsumer {
-  constructor() {
-    this.bincryInteract = new BincryInteract('YOUR_BINANCE_API_KEY', 'YOUR_BINANCE_API_SECRET');
+class BincryInteract {
+  constructor(binanceApiKey, binanceApiSecret, ethereumNodeUrl) {
+    this.binanceApiKey = binanceApiKey;
+    this.binanceApiSecret = binanceApiSecret;
+    this.web3 = new Web3(new Web3.providers.HttpProvider(ethereumNodeUrl));
+    this.binanceApiBase = 'https://api.binance.com';
+    this.binanceHeaders = {
+      'X-MBX-APIKEY': this.binanceApiKey,
+    };
   }
 
-  async runConsumerLogic() {
+  async getBinanceAccountInfo() {
+    const endpoint = '/api/v3/account';
+    const url = `${this.binanceApiBase}${endpoint}`;
+    const params = {
+      timestamp: Date.now(),
+      // Additional parameters for signed endpoint
+    };
+    // Signature process here, omitted for brevity
+    const config = {
+      headers: this.binanceHeaders,
+      params,
+    };
     try {
-      const binanceAccountInfo = await this.bincryInteract.getBinanceAccountInfo();
-      console.log('Binance Account Info in Consumer:', binanceAccountInfo);
-
-      const ethBalance = await this.bincryInteract.getEthBalance('0x5FbDB2315678afecb367f032d93F642f64180aa3');
-      console.log('Ethereum Balance in Consumer:', ethBalance);
-
-      const contractInteractionResult = await this.bincryInteract.interactWithSmartContract(
-        '0xContractAddress',
-        '0xYourPrivateKey',
-        'yourSmartContractFunction',
-        ['arg1', 'arg2']
-      );
-
-      console.log('Contract Interaction Result in Consumer:', contractInteractionResult);
+      const response = await axios.get(url, config);
+      return response.data;
     } catch (error) {
-      console.error('Error in Consumer:', error.message);
+      console.error('Error fetching Binance account info:', error);
+      throw error;
+    }
+  }
+
+  async getEthBalance(address) {
+    try {
+      const balance = await this.web3.eth.getBalance(address);
+      return this.web3.utils.fromWei(balance, 'ether');
+    } catch (error) {
+      console.error('Error fetching Ethereum balance:', error);
+      throw error;
+    }
+  }
+
+  async interactWithSmartContract(contractAddress, privateKey, functionName, args) {
+    // Simplified ERC-20 ABI
+    const erc20ABI = [
+      {
+        "constant": true,
+        "inputs": [{"name":"_owner","type":"address"}],
+        "name": "balanceOf",
+        "outputs": [{"name":"balance","type":"uint256"}],
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],
+        "name": "transfer",
+        "outputs": [{"name":"","type":"bool"}],
+        "type": "function"
+      }
+    ];
+
+    const contract = new this.web3.eth.Contract(erc20ABI, contractAddress);
+    const account = this.web3.eth.accounts.privateKeyToAccount(privateKey);
+    this.web3.eth.accounts.wallet.add(account);
+    this.web3.eth.defaultAccount = account.address;
+
+    try {
+      let transaction;
+      if (functionName === "balanceOf") {
+        const balance = await contract.methods[functionName](...args).call();
+        return balance;
+      } else if (functionName === "transfer") {
+        const gasPrice = await this.web3.eth.getGasPrice();
+        const gasEstimate = await contract.methods[functionName](...args).estimateGas({ from: account.address });
+        transaction = await contract.methods[functionName](...args).send({
+          from: account.address,
+          gas: gasEstimate,
+          gasPrice
+        });
+      } else {
+        throw new Error("Unsupported function");
+      }
+
+      return transaction;
+    } catch (error) {
+      console.error('Error interacting with smart contract:', error);
+      throw error;
     }
   }
 }
 
-// Usage
-const bincryConsumer = new BincryConsumer();
-bincryConsumer.runConsumerLogic();
+module.exports = BincryInteract;
